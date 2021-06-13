0575cd0fc6eb65dc6778c318855d2c85168bba67
seiji <pillow1207@gmail.com>
2021-06-13 11:19:17 +0900

    エンティティクラスの作成。

---
+++ src/main/java/com/example/Entity/User.java
@@ -0,0 +1,28 @@
+package com.example.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="user")
+public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String address;
+    @Column
+    private String tel;
+}
